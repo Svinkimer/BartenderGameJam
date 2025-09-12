5baf8d8cce4f81bd27b4fe826d7aa502e6673256
svinkimer <aleksandrkrepkovic@gmail.com>
2025-09-12 13:33:21 +0300

    Basic addition of ingridients

--- Scripts/Bottle_Script.gd
+++ Scripts/Bottle_Script.gd
@@ -14,4 +14,6 @@ func _on_mouse_exited() -> void:
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	#if (event == )
+	if (event is InputEventMouseButton and event.pressed):
+		print("Bottle " + key + " was used.")
+		GameState.add_drink_in_mixer(key)
--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -1,6 +1,31 @@
 extends Node
 
-var drinks_in_mixer: Array[StringName]
+
+#region MANAGEMENT OF INGREDIENTS
+const MAX_DRINKS_IN_MIX: int = 3
+
+var drinks_in_mixer: Array[StringName] = []
+
+func add_drink_in_mixer(key: StringName) -> bool:
+	# mixer cup is full
+	if len(drinks_in_mixer) >= MAX_DRINKS_IN_MIX:
+		print("Mixer is full")
+		return false;
+		
+	for drink in drinks_in_mixer:
+		# same drink -> nothing happens
+		if key == drink:
+			print("This drink was already poured in shake")
+			return true;
+	
+	drinks_in_mixer.push_front(key);
+	
+	print(drinks_in_mixer)
+	
+	return true
+
+
+#endregion
 
 
 
