d8ce22bd8e17ccf99e37f3748f301ab86fa1e26b
svinkimer <aleksandrkrepkovic@gmail.com>
2025-09-12 10:15:14 +0300

    First Commit of this prject

---
+++ Bottle_Script.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var key: StringName 
+
+func _on_mouse_entered() -> void:
+	
+	print("Mouse entered bottle!")
+
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
