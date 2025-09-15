cd3ec164600be6d900ab1b68ea9b864fc928dd66
AlyaLyo <egorovaAI03@gmail.com>
2025-09-15 21:17:03 +0300

    feat

--- Scripts/base_scene_script.gd
+++ Scripts/base_scene_script.gd
@@ -5,3 +5,4 @@ func _ready() -> void:
 	GameState.alien_spawning_point_position = $AlienSpawningPoint.global_position
 	GameState.ordered_cocktail_label = $OrderedCocktailLabel
 	GameState.mixed_cocktail_label = $MixedCocktailLabel
+	#
