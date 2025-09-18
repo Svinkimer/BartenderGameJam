7166f68bdfb141fb14766e071b0a565305e00097
anatole <feshiardragociy@mail.ru>
2025-09-18 20:10:31 +0300

    tutorial formatting

--- Scripts/tutorial.gd
+++ Scripts/tutorial.gd
@@ -12,31 +12,31 @@ func create_bar_menu():
 	bar_menu += "[table=4][cell=1][center][b]Кровавый Йода[/b][/center][/cell][cell=3][/cell]"
 	bar_menu += "[cell=1][indent][indent][indent][center][b]Черный русский[/b][/center][/indent][/indent][/indent][/cell][cell=3][/cell]"
 	bar_menu += "[cell][img=256x256]res://Assets/Graphics/Tutorial/cocktail1.png[/img][/cell]"
-	bar_menu += "[cell][indent]• Кровь ОМЕЖки\n• Силушка богатырская\n[/indent][/cell]\n\n"
+	bar_menu += "[cell][indent]• Кровь\nОМЕЖки\n• Силушка\nбогатырская\n[/indent][/cell]\n\n"
 	bar_menu += "[cell][indent][indent][indent][center][img=256x256]res://Assets/Graphics/Tutorial/cocktail2.png[/img][/center][/indent][/indent][/indent][/cell]"
-	bar_menu += "[cell][indent][indent]• Силушка богатырская\n• 50 abCent\n[/indent][/indent][/cell][/table]\n\n"
+	bar_menu += "[cell][indent][indent]• Силушка\nбогатырская\n• 50 abCent\n[/indent][/indent][/cell][/table]\n\n"
 	
 	# Cocktails 3 & 4
-	bar_menu += "[table=4][cell][center][b]Копуляция на Татуине[/b][/center][/cell][cell][/cell]"
+	bar_menu += "[table=4][cell][center][b]Копуляция\nна Татуине[/b][/center][/cell][cell][/cell]"
 	bar_menu += "[cell][indent][indent][indent][center][b]Джедай-тоник[/b][/center][/indent][/indent][/indent][/cell][cell][/cell]"
 	bar_menu += "[cell][img=256x256]res://Assets/Graphics/Tutorial/cocktail3.png[/img][/cell]"
-	bar_menu += "[cell][indent]• Кровь ОМЕЖки\n• 50 abCent\n[/indent][/cell]\n\n"
+	bar_menu += "[cell][indent]• Кровь\nОМЕЖки\n• 50 abCent\n[/indent][/cell]\n\n"
 	bar_menu += "[cell][indent][indent][indent][center][img=256x256]res://Assets/Graphics/Tutorial/cocktail4.png[/img][/center][/indent][/indent][/indent][/cell]"
-	bar_menu += "[cell][indent][indent]• Силушка богатырская\n• Силушка богатырская\n[/indent][/indent][/cell][/table]\n\n"
+	bar_menu += "[cell][indent][indent]• Силушка\nбогатырская\n• Силушка\nбогатырская\n[/indent][/indent][/cell][/table]\n\n"
 	
 	# Cocktails 5 & 6
 	bar_menu += "[table=4][cell][center][b]Хиросима[/b][/center][/cell][cell][/cell]"
 	bar_menu += "[cell][indent][indent][indent][center][b]Third sunrise[/b][/center][/indent][/indent][/indent][/cell][cell][/cell]"
 	bar_menu += "[cell][img=256x256]res://Assets/Graphics/Tutorial/cocktail5.webp[/img][/cell]"
-	bar_menu += "[cell][indent]• Силушка богатырская\n• Жижа\n[/indent][/cell]\n\n"
+	bar_menu += "[cell][indent]• Силушка\nбогатырская\n• Жижа\n[/indent][/cell]\n\n"
 	bar_menu += "[cell][indent][indent][indent][center][img=256x256]res://Assets/Graphics/Tutorial/cocktail1.png[/img][/center][/indent][/indent][/indent][/cell]"
-	bar_menu += "[cell][indent][indent]• Кровь ОМЕЖки\n• Жижа\n[/indent][/indent][/cell][/table]\n\n"
+	bar_menu += "[cell][indent][indent]• Кровь\nОМЕЖки\n• Жижа\n[/indent][/indent][/cell][/table]\n\n"
 	
 	# Cocktails 7 & 8
-	bar_menu += "[table=4][cell][center][b]Проект Alien-хэттен[/b][/center][/cell][cell][/cell]"
+	bar_menu += "[table=4][cell][center][b]Проект\nAlien-хэттен[/b][/center][/cell][cell][/cell]"
 	bar_menu += "[cell][indent][indent][indent][center][b]Виски-кола[/b][/center][/indent][/indent][/indent][/cell][cell][/cell]"
 	bar_menu += "[cell][img=256x256]res://Assets/Graphics/Tutorial/cocktail2.png[/img][/cell]"
-	bar_menu += "[cell][indent]• Кровь ОМЕЖки\n• Кровь ОМЕЖки\n[/indent][/cell]\n\n"
+	bar_menu += "[cell][indent]• Кровь\nОМЕЖки\n• Кровь\nОМЕЖки\n[/indent][/cell]\n\n"
 	bar_menu += "[cell][indent][indent][indent][center][img=256x256]res://Assets/Graphics/Tutorial/cocktail3.png[/img][/center][/indent][/indent][/indent][/cell]"
 	bar_menu += "[cell][indent][indent]• 50 abCent\n• Жижа\n[/indent][/indent][/cell][/table]\n\n"
 	
@@ -48,9 +48,9 @@ func create_bar_menu():
 	bar_menu += "[cell][img=256x256]res://Assets/Graphics/Topping_1.png[/img][/cell]"
 	bar_menu += "[cell][indent]НЕ смешивать с:\n• Хиросима\n• Черный русский\n[/indent][/cell]\n\n"
 	bar_menu += "[cell][indent][indent][center][img=256x256]res://Assets/Graphics/Topping_2.png[/img][/center][/indent][/indent][/cell]"
-	bar_menu += "[cell][indent][indent]НЕ смешивать с:\n• Копуляция на Татуине\n• Third sunrise\n[/indent][/indent][/cell][/table]\n\n"
+	bar_menu += "[cell][indent][indent]НЕ смешивать с:\n• Копуляция\nна Татуине\n• Third sunrise\n[/indent][/indent][/cell][/table]\n\n"
 	
-	bar_menu += "[font_size=10]Make sure to pay your taxes by pressing 'N' before serving your every 10th customer.[/font_size]\n"
+	bar_menu += "[font_size=10]Make sure to pay your taxes by pressing 'N' before serving your every 5th customer.[/font_size]\n"
 	
 	rich_text_label.bbcode_text = bar_menu
 
